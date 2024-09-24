c2a08bff68b68eeebabc3f3ba30da3d7805a93e5
alvrs <alvarius@lattice.xyz>
2024-09-24 14:21:43 +0200

    set up basic tests

--- packages/contracts/test/PaymasterTest.t.sol
+++ packages/contracts/test/PaymasterTest.t.sol
@@ -4,10 +4,20 @@ pragma solidity >=0.8.24;
 import "forge-std/Test.sol";
 import { MudTest } from "@latticexyz/world/test/MudTest.t.sol";
 import { getKeysWithValue } from "@latticexyz/world-modules/src/modules/keyswithvalue/getKeysWithValue.sol";
+import { EntryPoint } from "@account-abstraction/contracts/core/EntryPoint.sol";
 
 import { IWorld } from "../src/codegen/world/IWorld.sol";
 
 contract PaymasterTest is MudTest {
+  EntryPoint entryPoint;
+  IWorld paymaster;
+
+  function setUp() public override {
+    super.setUp();
+    entryPoint = new EntryPoint();
+    paymaster = IWorld(worldAddress);
+  }
+
   function testWorldExists() public {
     uint256 codeSize;
     address addr = worldAddress;
