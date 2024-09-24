1d91aea3c51857156e17de6c658599d027ebcac1
alvrs <alvarius@lattice.xyz>
2024-09-24 14:04:26 +0200

    some cleanup

--- packages/contracts/mud.config.ts
+++ packages/contracts/mud.config.ts
@@ -1,13 +1,3 @@
 import { defineWorld } from "@latticexyz/world";
 
-export default defineWorld({
-  namespace: "app",
-  tables: {
-    Counter: {
-      schema: {
-        value: "uint32",
-      },
-      key: [],
-    },
-  },
-});
+export default defineWorld({});
