415d655aaabd6ab28315719514a01d68f2e6e7f1
alvrs <alvarius@lattice.xyz>
2024-10-03 23:35:51 +0100

    add script to fund paymaster

--- packages/contracts/script/FundPaymaster.s.sol
+++ packages/contracts/script/FundPaymaster.s.sol
@@ -9,19 +9,19 @@ import { IEntryPoint } from "@account-abstraction/contracts/interfaces/IEntryPoi
 import { IWorld } from "../src/codegen/world/IWorld.sol";
 import { SystemConfig } from "../src/namespaces/root/codegen/tables/SystemConfig.sol";
 
-contract PostDeploy is Script {
+contract FundPaymaster is Script {
   function run(address worldAddress) external {
     // Specify a store so that you can use tables directly in PostDeploy
     StoreSwitch.setStoreAddress(worldAddress);
 
     // Load the private key from the `PRIVATE_KEY` environment variable (in .env)
     uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-    IEntryPoint entryPoint07 = IEntryPoint(0x0000000071727De22E5E9d8BAf0edAc6f37da032);
+    IEntryPoint entryPoint = IEntryPoint(0x0000000071727De22E5E9d8BAf0edAc6f37da032);
 
     // Start broadcasting transactions from the deployer account
     vm.startBroadcast(deployerPrivateKey);
 
-    SystemConfig.set(address(entryPoint07));
+    entryPoint.depositTo{ value: 0.1 ether }(worldAddress);
 
     vm.stopBroadcast();
   }
