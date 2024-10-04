72802e156c503015c3f437f5c7844deab2ce59c3
alvrs <alvarius@lattice.xyz>
2024-10-04 15:12:10 +0100

    connect with counter for demo

--- packages/client/src/actions.ts
+++ packages/client/src/actions.ts
@@ -1,10 +1,11 @@
 import { getAction, parseEther } from "viem/utils";
-import { adminClient, grantorClient, publicClient, smartAccountClient, userClient } from "./clients";
+import { adminClient, grantorClient, publicClient, smartAccountClient } from "./clients";
 import { waitForTransactionReceipt, writeContract } from "viem/actions";
 import { paymaster } from "./contract";
-import { Address, Hex } from "viem";
+import { Address, Hex, parseAbi } from "viem";
 import { resourceToHex } from "@latticexyz/common";
 import { sendUserOperation } from "viem/account-abstraction";
+import { chain } from "./chain";
 
 export async function setGrantAllowance(amount: bigint) {
   const txHash = await getAction(
@@ -38,11 +39,7 @@ export async function grantAllowance(amount: bigint, receiver: Address) {
   console.log("Grant allowance", receipt);
 }
 
-export async function registerPass(passId: Hex) {
-  const claimAmount = parseEther("0.01");
-  const claimInterval = 5n;
-  const validityPeriod = 30n;
-
+export async function registerPass(passId: Hex, claimAmount: bigint, claimInterval: bigint, passValidity: bigint) {
   const txHash = await getAction(
     grantorClient,
     writeContract,
@@ -51,7 +48,7 @@ export async function registerPass(passId: Hex) {
     ...paymaster,
     account: grantorClient.account,
     functionName: "registerPass",
-    args: [passId, claimAmount, claimInterval, validityPeriod],
+    args: [passId, claimAmount, claimInterval, passValidity],
   });
 
   const receipt = await waitForTransactionReceipt(publicClient, { hash: txHash });
@@ -92,21 +89,6 @@ export async function claim(passId: Hex, receiver: Address) {
 }
 
 export async function registerNamespace(namespace: string) {
-  // const txHash = await getAction(
-  //   smartAccountClient,
-  //   writeContract,
-  //   "writeContract",
-  // )({
-  //   ...paymaster,
-  //   account: smartAccountClient.account,
-  //   functionName: "registerNamespace",
-  //   args: [resourceToHex({ type: "namespace", namespace, name: "" })],
-  //   preVerificationGas: 1_000_000,
-  //   verificationGas: 1_000_000,
-  // });
-
-  // const receipt = await waitForTransactionReceipt(publicClient, { hash: txHash });
-
   const result = await getAction(
     smartAccountClient,
     sendUserOperation,
@@ -126,4 +108,31 @@ export async function registerNamespace(namespace: string) {
   });
 
   console.log("Register namespace", result);
+}
+
+export async function incrementCounter() {
+  if (!("counter" in chain.contracts)) {
+    console.log("counter not supported on this chain");
+    return;
+  }
+
+  const result = await getAction(
+    smartAccountClient,
+    sendUserOperation,
+    "sendUserOperation",
+  )({
+    calls: [
+      {
+        abi: parseAbi(["function increment() returns (uint256)"]),
+        to: chain.contracts.counter.address,
+        functionName: "increment",
+        args: [],
+      },
+    ],
+    preVerificationGas: 100_000n,
+    verificationGasLimit: 1_000_000n,
+    callGasLimit: 1_000_000n,
+  });
+
+  console.log("Increment counter", result);
 }
--- packages/client/src/clients.ts
+++ packages/client/src/clients.ts
@@ -7,20 +7,19 @@ import {
   ClientConfig,
   PublicClient,
   Hex,
+  HttpTransport,
 } from "viem";
 import { createBurnerAccount } from "@latticexyz/common";
 import { transactionQueue } from "@latticexyz/common/actions";
 import { observer } from "@latticexyz/explorer/observer";
-import { entryPoint06Address } from "viem/account-abstraction";
+import { entryPoint06Address, SmartAccount } from "viem/account-abstraction";
 import { worldAddress as paymasterAddress } from "contracts/deploys/31337/latest.json";
 import { toSimpleSmartAccount } from "permissionless/accounts";
 import { createSmartAccountClient, SmartAccountClient } from "permissionless";
 import { chain } from "./chain";
 
-// for demo - 1st anvil key
+// for demo - 1st and 2nd anvil key
 const adminKey = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
-
-// for demo - 2nd anvil key
 const grantorKey = "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d";
 
 // for demo - unfunded key
@@ -28,7 +27,7 @@ const userKey = "0xab824ae96b4d6bf50deddfc4ec7fe0ed54ce3e601277239c41f4630b98e5c
 
 const clientOptions = {
   chain,
-  transport: fallback([webSocket(), http()]),
+  transport: http(), // fallback([webSocket(), http()]),
   pollingInterval: 10,
 } as const satisfies ClientConfig;
 
@@ -55,7 +54,7 @@ export const userClient = createWalletClient({
   .extend(transactionQueue())
   .extend(observer());
 
-const smartAccount = await toSimpleSmartAccount({
+const smartAccount: SmartAccount = await toSimpleSmartAccount({
   client: publicClient,
   // entryPoint: { address: entryPoint07Address, version: "0.7" },
   entryPoint: { address: entryPoint06Address, version: "0.6" },
@@ -64,15 +63,16 @@ const smartAccount = await toSimpleSmartAccount({
   owner: createBurnerAccount(userKey),
 });
 
-export const smartAccountClient: SmartAccountClient = createSmartAccountClient({
-  chain,
-  account: smartAccount,
-  client: publicClient,
-  bundlerTransport: http(chain.rpcUrls.erc4337.http[0]),
-  paymaster: {
-    async getPaymasterData() {
-      // return { paymaster: paymasterAddress as Hex, paymasterData: "0x" };
-      return { paymasterAndData: paymasterAddress as Hex };
+export const smartAccountClient: SmartAccountClient<HttpTransport, typeof chain, typeof smartAccount> =
+  createSmartAccountClient({
+    chain,
+    account: smartAccount,
+    client: publicClient,
+    bundlerTransport: http(chain.rpcUrls.erc4337.http[0]),
+    paymaster: {
+      async getPaymasterData() {
+        // return { paymaster: paymasterAddress as Hex, paymasterData: "0x" };
+        return { paymasterAndData: paymasterAddress as Hex };
+      },
     },
-  },
-});
+  });
