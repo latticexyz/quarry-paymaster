882daad8e3150b02ca7e932566958490f6ff183e
alvrs <alvarius@lattice.xyz>
2024-10-04 12:12:28 +0100

    add rhodolite, clean up types

--- packages/client/src/contract.ts
+++ packages/client/src/contract.ts
@@ -1,6 +1,4 @@
-import { Hex } from "viem";
 import WorldABI from "contracts/out/IWorld.sol/IWorld.abi.json";
-import worldDeployment from "contracts/deploys/31337/latest.json";
-import { anvil } from "viem/chains";
+import { chain } from "./chain";
 
-export const paymaster = { abi: WorldABI, address: worldDeployment.worldAddress as Hex, chain: anvil };
+export const paymaster = { abi: WorldABI, address: chain.contracts.quarryPaymaster.address, chain };
