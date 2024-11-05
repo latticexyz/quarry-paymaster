b2baabe5e5d4e91f1339160e167cb936e08c1202
alvrs <alvarius@lattice.xyz>
2024-11-05 15:09:42 +0700

    make fund paymaster script idempotent

--- packages/contracts/script/FundPaymaster.s.sol
+++ packages/contracts/script/FundPaymaster.s.sol
@@ -11,18 +11,18 @@ import { SystemConfig } from "../src/namespaces/root/codegen/tables/SystemConfig
 
 contract FundPaymaster is Script {
   function run(address worldAddress) external {
-    // Specify a store so that you can use tables directly in PostDeploy
     StoreSwitch.setStoreAddress(worldAddress);
-
-    // Load the private key from the `PRIVATE_KEY` environment variable (in .env)
     uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
     IEntryPoint entryPoint = IEntryPoint(0x0000000071727De22E5E9d8BAf0edAc6f37da032);
-
-    // Start broadcasting transactions from the deployer account
-    vm.startBroadcast(deployerPrivateKey);
-
-    entryPoint.depositTo{ value: 1 ether }(worldAddress);
-
-    vm.stopBroadcast();
+    uint256 targetBalance = 5 ether;
+    uint256 balance = entryPoint.balanceOf(worldAddress);
+    console.log("Current paymaster balance: %s", balance);
+    if (balance < targetBalance) {
+      uint256 amount = targetBalance - balance;
+      console.log("Funding paymaster with %s", amount);
+      vm.startBroadcast(deployerPrivateKey);
+      entryPoint.depositTo{ value: amount }(worldAddress);
+      vm.stopBroadcast();
+    }
   }
 }
