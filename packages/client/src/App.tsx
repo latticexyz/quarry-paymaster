fb4fc72b6c6333947c38e223af2893973eb19828
alvarius <alvarius@lattice.xyz>
2025-07-31 13:48:33 -0700

    upgrade to latest react template

---
+++ packages/client/src/App.tsx
@@ -0,0 +1,23 @@
+import { AccountButton } from "@latticexyz/entrykit/internal";
+import { Synced } from "./mud/Synced";
+
+export function App() {
+  return (
+    <>
+      <div className="fixed inset-0 grid place-items-center p-4">
+        <Synced
+          fallback={({ message, percentage }) => (
+            <div className="tabular-nums">
+              {message} ({percentage.toFixed(1)}%)…
+            </div>
+          )}
+        >
+          Dashboard
+        </Synced>
+      </div>
+      <div className="fixed top-2 right-2">
+        <AccountButton />
+      </div>
+    </>
+  );
+}
