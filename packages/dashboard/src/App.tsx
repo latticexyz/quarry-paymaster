877002afa2dd11ed353bfbe7820836003499bdb2
Kevin Ingersoll <kingersoll@gmail.com>
2025-08-01 16:39:34 +0900

    style sponsor form

--- packages/dashboard/src/App.tsx
+++ packages/dashboard/src/App.tsx
@@ -6,7 +6,10 @@ import { Sponsor } from "./Sponsor";
 export function App() {
   return (
     <>
-      <div className="fixed inset-0 grid place-items-center p-4">
+      <div className="sticky top-0 p-4 flex justify-end">
+        <AccountButton />
+      </div>
+      <div className="grid place-items-center p-16">
         <Synced
           fallback={({ message, percentage }) => (
             <div className="tabular-nums">
@@ -14,12 +17,30 @@ export function App() {
             </div>
           )}
         >
-          <Sponsor />
-          <Allowances />
+          <div className="grid grid-cols-2 gap-16">
+            <div className="border border-dashed border-neutral-600 p-8 space-y-8">
+              <div className="text-center space-y-2">
+                <h1 className="text-4xl font-mono text-white">Sponsor</h1>
+                <p className="max-w-72 text-sm text-pretty">
+                  Grant a gas allowance to other accounts, allowing them to
+                  spend some of your gas balance. You can claim back any unspent
+                  allowance at any time.
+                </p>
+              </div>
+              <Sponsor />
+            </div>
+            <div className="border border-dashed border-neutral-600 p-8 space-y-8">
+              <div className="text-center space-y-2">
+                <h1 className="text-4xl font-mono text-white">Allowances</h1>
+                <p className="max-w-72 text-sm text-pretty">
+                  Allowances you’ve received from sponsors. They are spent from
+                  lowest to highest.
+                </p>
+              </div>
+              <Allowances />
+            </div>
+          </div>
         </Synced>
-      </div>
-      <div className="fixed top-2 right-2">
-        <AccountButton />
       </div>
     </>
   );
--- packages/dashboard/src/Sponsor.tsx
+++ packages/dashboard/src/Sponsor.tsx
@@ -1,53 +1,121 @@
 import { useCallback, useState } from "react";
-import { getAddress } from "viem";
+import { Address, getAddress, isAddress, parseEther } from "viem";
 import { grantAllowance } from "./grantAllowance";
-import { useSessionClient } from "@latticexyz/entrykit/internal";
+import { SessionClient, useSessionClient } from "@latticexyz/entrykit/internal";
+import { useMutation } from "@tanstack/react-query";
+import { twMerge } from "tailwind-merge";
 
 export function Sponsor() {
-  const [user, setUser] = useState("");
-  const [allowance, setAllowance] = useState("");
-  const [error, setError] = useState<string | null>(null);
-  const [message, setMessage] = useState<string | null>(null);
   const { data: sessionClient } = useSessionClient();
 
-  const handleGrantAllowance = useCallback(async () => {
-    if (!sessionClient) return;
-    setError(null);
-    setMessage(null);
-    try {
-      const result = await grantAllowance({
-        receiver: getAddress(user),
+  const grant = useMutation({
+    mutationKey: ["grantAllowance", sessionClient?.uid],
+    async mutationFn({ formData }: { formData: FormData }) {
+      if (!sessionClient) throw new Error("Not connected.");
+
+      // TODO: parse form data with arktype or effect schema?
+
+      const receiver = formData.get("receiver");
+      if (typeof receiver !== "string" || !isAddress(receiver)) {
+        throw new Error("Invalid receiver address.");
+      }
+
+      const allowance = parseEther(formData.get("allowance") as string);
+      if (allowance <= 0) throw new Error("Invalid allowance.");
+
+      return await grantAllowance({
+        receiver: getAddress(receiver),
         allowance: BigInt(allowance),
         client: sessionClient,
       });
-      setMessage(result.message);
-    } catch (error) {
-      setError(error instanceof Error ? error.message : "Unknown error");
-    }
-  }, [user, allowance, sessionClient]);
+    },
+  });
 
   return (
-    <div>
-      <div>Sponsor</div>
-      <div>
+    <form
+      className="flex flex-col gap-2"
+      onSubmit={(event) => {
+        event.preventDefault();
+        grant.mutate({ formData: new FormData(event.currentTarget) });
+      }}
+    >
+      <label
+        className={twMerge(
+          "inline-flex border border-neutral-600 text-sm",
+          "focus-within:outline-2 focus-within:outline-blue-400/60"
+        )}
+      >
         <input
-          className="border-2 border-gray-300 rounded-md p-2"
-          type="text"
-          value={user}
-          onChange={(e) => setUser(e.target.value)}
+          name="receiver"
+          required
+          className="w-full p-2 outline-none"
+          placeholder="Receiver (Ethereum address)"
         />
-        <input
-          className="border-2 border-gray-300 rounded-md p-2"
-          type="number"
-          value={allowance}
-          onChange={(e) => setAllowance(e.target.value)}
-        />
-        <button disabled={!sessionClient} onClick={handleGrantAllowance}>
-          Give
-        </button>
-        {error && <div className="text-red-500">{error}</div>}
-        {message && <div>{message}</div>}
+      </label>
+      <div className="flex flex-col gap-1">
+        <label
+          className={twMerge(
+            "inline-flex border border-neutral-600 text-sm",
+            "focus-within:outline-2 focus-within:outline-blue-400/60"
+          )}
+        >
+          <input
+            name="allowance"
+            required
+            className="w-full p-2 outline-none"
+            placeholder="Allowance amount"
+          />
+          <div className="m-1 p-2 text-xs leading-none bg-neutral-700 text-white inline-flex items-center">
+            ETH
+          </div>
+        </label>
+        <div className="flex gap-1">
+          {["0.005", "0.01", "0.05", "0.1"].map((amount) => (
+            <button
+              key={amount}
+              type="button"
+              className="grow p-1.5 text-xs leading-none bg-neutral-500 hover:brightness-125 active:brightness-90 text-white cursor-pointer focus:outline-2 focus:outline-blue-400/60"
+              onClick={(event) => {
+                const input = event.currentTarget.form?.allowance;
+                if (!input) throw new Error("Could not find allowance input.");
+
+                input.value = amount;
+                input.focus();
+              }}
+            >
+              {amount}&nbsp;ETH
+            </button>
+          ))}
+        </div>
       </div>
-    </div>
+      <button
+        type="submit"
+        className={twMerge(
+          "group",
+          "bg-orange-500 hover:brightness-125 active:brightness-90 text-white font-medium cursor-pointer",
+          "disabled:pointer-events-none disabled:opacity-40 disabled:grayscale",
+          "aria-busy:pointer-events-none aria-busy:animate-pulse",
+          "focus:outline-2 focus:outline-blue-400/60"
+        )}
+        disabled={!sessionClient}
+        aria-busy={grant.isPending}
+      >
+        <span className="p-2 inline-grid *:col-start-1 *:row-start-1 overflow-clip">
+          <span className="transition group-aria-busy:-translate-y-2 group-aria-busy:opacity-0">
+            Grant allowance
+          </span>
+          <span
+            className="transition translate-y-4 group-aria-busy:translate-y-0 not-group-aria-busy:opacity-0"
+            aria-hidden
+          >
+            ⋯
+          </span>
+        </span>
+      </button>
+      {grant.error ? (
+        <div className="text-red-500">{grant.error.message}</div>
+      ) : null}
+      {grant.data ? <div>{grant.data.message}</div> : null}
+    </form>
   );
 }
