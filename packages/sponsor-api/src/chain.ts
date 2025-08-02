915f61c45c72158889440e135c526d0d4b021d1e
alvarius <alvarius@lattice.xyz>
2025-08-02 07:39:40 +0200

    fix: add paymaster and bundler url to redstone chain config (#18)

--- packages/sponsor-api/src/chain.ts
+++ packages/sponsor-api/src/chain.ts
@@ -1,11 +1,11 @@
-import { anvil } from "viem/chains";
+import { anvil as anvilConfig } from "viem/chains";
 import { Chain } from "viem";
 import worlds from "contracts/worlds.json";
 import env from "./env";
-import { garnet } from "./garnet";
+import { garnet, pyrope, redstone as redstoneConfig } from "@latticexyz/common/chains";
 
 const anvilWithPaymaster = {
-  ...anvil,
+  ...anvilConfig,
   contracts: {
     quarryPaymaster: {
       address: worlds[31337]!.address,
@@ -14,7 +14,23 @@ const anvilWithPaymaster = {
   },
 } satisfies Chain;
 
-const supportedChains = [anvilWithPaymaster, garnet];
+const redstone = {
+  ...redstoneConfig,
+  rpcUrls: {
+    ...redstoneConfig.rpcUrls,
+    bundler: {
+      http: ["https://rpc.redstonechain.com"],
+    },
+  },
+  contracts: {
+    quarryPaymaster: {
+      address: worlds[690]!.address,
+      blockCreated: worlds[690]!.blockNumber,
+    },
+  },
+} satisfies Chain;
+
+const supportedChains = [anvilWithPaymaster, garnet, pyrope, redstone];
 
 const chain = supportedChains.find((c) => c.id === Number(env.CHAIN_ID))!;
 if (!chain) {
